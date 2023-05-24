f2a2897f2bf5b26a2df09034d9a25912b9730d37
haval95 <havalrezan22@gmail.com>
2023-05-24 22:32:33 +0300

    FEAT: API END POINTS CREATED
    end points for both category and menut item created to read, update and delete them

--- littlelemon/littleLemonAPI/views.py
+++ littlelemon/littleLemonAPI/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Menu_Item, Category
+from .serializers import CategorySerializer, MenuItemSerializer
 
-# Create your views here.
+
+class CategoryView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer 
+
+
+class SingleCategoryView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class Menu_ItemView(generics.ListCreateAPIView):
+    queryset = Menu_Item.objects.all()
+    serializer_class = MenuItemSerializer
+    
+    
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu_Item.objects.all()
+    serializer_class = MenuItemSerializer
