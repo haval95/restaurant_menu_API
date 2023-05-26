d0775ccc44030c1f1e724663f519e7a2c82e3910
haval95 <havalrezan22@gmail.com>
2023-05-26 11:39:40 +0300

    category id to PROTECT in menu item

--- littlelemon/littleLemonAPI/models.py
+++ littlelemon/littleLemonAPI/models.py
@@ -15,7 +15,7 @@ class Menu_Item(models.Model):
     item_price = models.IntegerField()
     image = models.ImageField(upload_to='images', null=True)
     category_id = models.ForeignKey(
-        Category, related_name="category", on_delete= models.CASCADE, default=None
+        Category, related_name="category", on_delete= models.PROTECT, default=None
     ) 
     inventory = models.SmallIntegerField()   
 
