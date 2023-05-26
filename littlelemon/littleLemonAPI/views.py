207c8c207b91428855b1d654d1338b6ada3e610d
haval95 <havalrezan22@gmail.com>
2023-05-26 19:16:20 +0300

    filtering and sorting

--- littlelemon/littleLemonAPI/views.py
+++ littlelemon/littleLemonAPI/views.py
@@ -12,16 +12,51 @@ def menu_items_fn_view(request):
         category_name = request.query_params.get("category")
         to_price = request.query_params.get("to_price")
         search = request.query_params.get("search")
+        ordering = request.query_params.get("ordering")
         if category_name:
             items = items.filter(category__category_name=category_name)
         if to_price:
             items = items.filter(item_price__lte= to_price)
         if search:
             items = items.filter(item_name__icontains= search)
+        if ordering:
+            ordering_feilds = ordering.split(",")
+            items = items.order_by(*ordering_feilds)
         
         serialized_items = MenuItemSerializer(items, many=True)
         return Response(serialized_items.data)
+    
+    
+class Menu_ItemView(generics.ListCreateAPIView):
+    queryset = Menu_Item.objects.select_related('category').all()
+    serializer_class = MenuItemSerializer
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category_name = self.request.query_params.get("category")
+        to_price = self.request.query_params.get("to_price")
+        search = self.request.query_params.get("search")
+        ordering = self.request.query_params.get("ordering")
+
+        if category_name:
+            queryset = queryset.filter(category__category_name=category_name)
+        if to_price:
+            queryset = queryset.filter(item_price__lte=to_price)
+        if search:
+            queryset = queryset.filter(item_name__icontains=search)
+        if ordering:
+            ordering_feilds = ordering.split(",")
+            queryset = queryset.order_by(*ordering_feilds)
+
+        return queryset
+
         
+    
+    
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu_Item.objects.all()
+    serializer_class = MenuItemSerializer
+    
         
 
 class CategoryView(generics.ListCreateAPIView):
@@ -32,31 +67,3 @@ class CategoryView(generics.ListCreateAPIView):
 class SingleCategoryView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-
-
-class Menu_ItemView(generics.ListCreateAPIView):
-    queryset = Menu_Item.objects.select_related('category').all()
-    serializer_class = MenuItemSerializer
-    
-    def filter_queryset(self, queryset):
-        category_name = self.request.query_params.get("category")
-        to_price = self.request.query_params.get("to_price")
-        search = self.request.query_params.get("search")
-        if category_name:
-            queryset = queryset.filter(category__category_name=category_name)
-        if to_price:
-            queryset = queryset.filter(item_price__lte=to_price)
-        if search:
-            queryset = queryset.filter(item_name__icontains=search)
-        return queryset
-
-    def list(self, request):
-        queryset = self.filter_queryset(self.queryset)
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
-        
-    
-    
-class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
-    queryset = Menu_Item.objects.all()
-    serializer_class = MenuItemSerializer
