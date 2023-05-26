868696dc122c060d65ab83a5d68232b9b04b7ee2
haval95 <havalrezan22@gmail.com>
2023-05-26 18:34:33 +0300

    FEAT: TWO different ways to add filteration using prams

--- littlelemon/littleLemonAPI/urls.py
+++ littlelemon/littleLemonAPI/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("menu-items", views.Menu_ItemView.as_view()),
+    path("menu-items-fn", views.menu_items_fn_view),
     path("categories", views.CategoryView.as_view()),
     path("menu-items/<int:pk>", views.SingleMenuItemView.as_view()),
     path("categories/<int:pk>", views.SingleCategoryView.as_view()),
--- littlelemon/littleLemonAPI/views.py
+++ littlelemon/littleLemonAPI/views.py
@@ -1,7 +1,28 @@
 from rest_framework import generics
 from .models import Menu_Item, Category
 from .serializers import CategorySerializer, MenuItemSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
+@api_view(["GET"])
+def menu_items_fn_view(request):
+    if request.method == "GET":
+        items = Menu_Item.objects.select_related("category").all()
+        category_name = request.query_params.get("category")
+        to_price = request.query_params.get("to_price")
+        search = request.query_params.get("search")
+        if category_name:
+            items = items.filter(category__category_name=category_name)
+        if to_price:
+            items = items.filter(item_price__lte= to_price)
+        if search:
+            items = items.filter(item_name__icontains= search)
+        
+        serialized_items = MenuItemSerializer(items, many=True)
+        return Response(serialized_items.data)
+        
+        
 
 class CategoryView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
@@ -16,6 +37,24 @@ class SingleCategoryView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
 class Menu_ItemView(generics.ListCreateAPIView):
     queryset = Menu_Item.objects.select_related('category').all()
     serializer_class = MenuItemSerializer
+    
+    def filter_queryset(self, queryset):
+        category_name = self.request.query_params.get("category")
+        to_price = self.request.query_params.get("to_price")
+        search = self.request.query_params.get("search")
+        if category_name:
+            queryset = queryset.filter(category__category_name=category_name)
+        if to_price:
+            queryset = queryset.filter(item_price__lte=to_price)
+        if search:
+            queryset = queryset.filter(item_name__icontains=search)
+        return queryset
+
+    def list(self, request):
+        queryset = self.filter_queryset(self.queryset)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+        
     
     
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
